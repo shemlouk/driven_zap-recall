9cbe801832ee1397a7d8c141d805885427f165a6
samuel <shemlouk@gmail.com>
2022-12-02 15:37:29 -0300

    feat: adicionado mensagem quando o jogo acaba

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,44 @@
 import styled from "styled-components";
 import cards from "../cards";
 import info from "../info.js";
+import partyEmoji from "../assets/img/party.png";
+import sadEmoji from "../assets/img/sad.png";
+
+const messages = [
+  {
+    emoji: sadEmoji,
+    title: "Putz...",
+    message: "Ainda faltam alguns...\nMas não desanime!",
+  },
+  {
+    emoji: partyEmoji,
+    title: "Parabéns!",
+    message: "Você não esqueceu de nenhum flashcard!",
+  },
+];
 
 export default function Footer({ finishedQuestions }) {
   const progress = finishedQuestions.length;
+  const isGameFinished = progress === cards.length;
+  const index = finishedQuestions.every((f) => f.score > 1) ? 1 : 0;
 
   return (
-    <Container progress={progress}>
+    <Container>
+      <Message isGameFinished={isGameFinished}>
+        <div>
+          <img src={messages[index].emoji} />
+          <span>{messages[index].title}</span>
+        </div>
+        <p>{messages[index].message}</p>
+      </Message>
       <p>
         {progress}/{cards.length} CONCLUÍDOS
       </p>
-      <div>
+      <ScoresLine progress={progress}>
         {finishedQuestions.map((f) => (
           <img key={f.id} src={info[f.score].file} />
         ))}
-      </div>
+      </ScoresLine>
     </Container>
   );
 }
@@ -36,11 +60,42 @@ const Container = styled.footer`
   color: #333333;
   padding: 10px 0px;
 
-  div {
-    display: ${({ progress }) => !progress && "none"};
-  }
-
   img {
     margin: 0px 3px;
   }
+`;
+
+const Message = styled.div`
+  display: ${({ isGameFinished }) => (isGameFinished ? "flex" : "none")};
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 15px;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 15px;
+  }
+
+  img {
+    width: 23px;
+    margin-right: 13px;
+    margin-left: 0px;
+  }
+
+  span {
+    font-weight: 700;
+  }
+
+  p {
+    width: 222px;
+    text-align: center;
+  }
+`;
+
+const ScoresLine = styled.div`
+  display: ${({ progress }) => !progress && "none"};
+  margin-top: 10px;
 `;
